2e3cfafdfc1d8d6ec130c2567f036640adddff75
Sandeep Kumar Mahato <sandeep20-391@aec.ac.in>
2026-03-30 18:26:34 +0530

    refactor: prevent multiple PrismaClient instances in development

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,14 @@
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+// Prevent multiple instances in development
+const globalForPrisma = global;
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  log: ['error'],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 module.exports = prisma;
